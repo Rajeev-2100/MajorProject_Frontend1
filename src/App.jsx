475acee993d02d699c1df8a5e302d1da2839942c
Rajeev Rawat <rajeevrawat2100@email.com>
2026-02-23 17:02:35 +0530

    fix: the frontend page of site and add favicon icon to the site

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,33 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Link } from "react-router-dom";
 
-
 function App() {
-
   return (
     <>
       <Header />
       <main className="container py-5">
-        <div className="d-flex justify-content-between align-items-center flex-wrap gap-4">
-          <Link to={`/category/Women`}>
-            <img
-              src="https://img.freepik.com/free-photo/happy-young-woman-uses-her-phone-posing-with-colorful-shopping-bags-studio_8353-5606.jpg"
-              className="img-fluid"
-              alt=""
-            />
-          </Link>
-          <Link to={`/category/Men`}>
-            <img
-              src="https://img.freepik.com/premium-photo/online-home-shopping-time-portrait-young-bearded-man-sitting-floor-with-mobile-phone-with-crossed-legs-with-shopping-bags_255757-7840.jpg"
-              className="img-fluid"
-              alt=""
-            />
-          </Link>
+        <div className="d-flex justify-content-between flex-row flex-wrap gap-4">
+          <div className="d-flex flex- column align-items-center flex-column">
+            <Link to={`/category/Women`}>
+              <img
+                src="https://img.freepik.com/free-photo/happy-young-woman-uses-her-phone-posing-with-colorful-shopping-bags-studio_8353-5606.jpg"
+                className="img-fluid"
+                alt=""
+              />
+            </Link>
+            <h4>Women</h4>
+          </div>
+          <div className="d-flex flex- column align-items-center flex-column">
+            {" "}
+            <Link to={`/category/Men`}>
+              <img
+                src="https://img.freepik.com/premium-photo/online-home-shopping-time-portrait-young-bearded-man-sitting-floor-with-mobile-phone-with-crossed-legs-with-shopping-bags_255757-7840.jpg"
+                className="img-fluid"
+                alt=""
+              />
+            </Link>
+            <h4>Men</h4>
+          </div>
         </div>
         <div className="py-5 d-flex justify-content-center flex-column align-items-center">
           <h1 className="pt-3 ">Ecommerce Website</h1>
